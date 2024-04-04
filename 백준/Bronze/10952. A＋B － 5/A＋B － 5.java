8141aef13a7257941f09a2a38032f9c2b9521a09
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-04-04 12:30:30 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int A = 0;
+		int B = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			
+			if(A==0&&B==0) {
+				break;
+			}
+			System.out.println(A+B);
+		}
+		
+		
+	}
+
+}
