ed19310c1195697b62b9dd3e3e9ba7087d8054e0
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-04-04 12:23:28 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 360 ms, Memory: 18712 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		sc.close();
+		
+		for(int i=1; i<=T; i++) {
+			for(int k=T; k>i; k--) {				
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
