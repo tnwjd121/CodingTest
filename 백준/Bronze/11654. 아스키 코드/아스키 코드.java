2ad60b043dd089086a2bf06c4e6836044cc2c1e5
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-04-12 15:50:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.next().charAt(0);
+		System.out.println(T);		
+	}
+}
