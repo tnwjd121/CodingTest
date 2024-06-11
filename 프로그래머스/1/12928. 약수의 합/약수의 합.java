051567208334bbfd9175732f802c576f4ec3f667
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-06-11 13:48:07 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n/2; i++){
+            if(n%i==0){
+                answer+=i;
+            }
+        }
+        answer= answer+n;
+        return answer;
+    }
+}
