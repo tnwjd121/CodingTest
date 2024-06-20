43dfa7193dbc80173579290511ebca5d7f368798
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-06-20 10:51:26 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        for(int num : d){
+            sum += num;
+            if(sum<=budget){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
