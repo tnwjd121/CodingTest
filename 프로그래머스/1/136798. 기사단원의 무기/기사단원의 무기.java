2cd2c5a50384126faa68a471741db1910999101c
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-06-27 15:40:02 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.71 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for(int i=1; i<=number;i++){
+            int num = 0;
+            for(int j=1; j*j<=i; j++){
+                if(j*j==i){
+                    num++;
+                }else if(i%j==0){
+                    num+=2;                    
+                }
+            }
+            if(num>limit){
+                answer +=power;
+            }else{
+                answer += num;
+            }
+        }
+        
+        return answer;
+    }
+}
